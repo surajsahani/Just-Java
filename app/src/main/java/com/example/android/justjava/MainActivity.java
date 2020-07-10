a23c63e4ff0384292a6a3c3a0e89ca572d428441
Suraj sahani <surajkumarsahani1997@gmail.com>
2020-07-10 23:52:04 +0530

    Adding intent

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.android.justjava;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.widget.EdgeEffectCompat;
 
@@ -27,19 +31,32 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        EditText nameField = (EditText)findViewById(R.id.name_field);
+        //Find the user number
+        EditText nameField = (EditText) findViewById(R.id.name_field);
         String name = nameField.getText().toString();
         Log.v("MainActivity", "Name: " + name);
-        CheckBox whipped_cream_checkbox =(CheckBox) findViewById(R.id.whipped_cream_checkbox);
-        CheckBox add_chocolate_checkbox =(CheckBox) findViewById(R.id.chocolate_checkbox);
-
+        //Figure out if the user want whipped cream topping
+        CheckBox whipped_cream_checkbox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
         boolean hasWhippedCream = whipped_cream_checkbox.isChecked();
-
+        //Figure out if the user want whipped cream topping
+        CheckBox add_chocolate_checkbox = (CheckBox) findViewById(R.id.chocolate_checkbox);
         boolean hasChocolate = add_chocolate_checkbox.isChecked();
 
-        int price =  calculatePrice(hasWhippedCream, hasChocolate);
-        displayMessage(createOrderSummary(name,price, hasWhippedCream, hasChocolate));
+        int price = calculatePrice(hasWhippedCream, hasChocolate);
+        String priceMessage = createOrderSummary(name, price, hasWhippedCream, hasChocolate);
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Just java order for" + name);
+        intent.putExtra(Intent.EXTRA_TEXT, priceMessage);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+
     }
+
+
 
     /**
      * This method displays the given quantity value on the screen.
@@ -75,6 +92,10 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the minus button is clicked.
      */
     public void increment(View view) {
+        if (quantity == 100){
+            Toast.makeText(this, "You cannot have more than 100 coffees", Toast.LENGTH_SHORT).show();
+            return;
+        }
         quantity = quantity + 1;
         displayQuantity(quantity);
     }
@@ -82,15 +103,13 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the plus button is clicked.
      */
     public void decrement(View view) {
+        if (quantity == 1){
+            Toast.makeText(this, "You cannot have less than 1 coffee", Toast.LENGTH_SHORT).show();
+            return;
+        }
         quantity = quantity -1;
         displayQuantity(quantity);
     }
-
-    private void displayMessage(String message) {
-        TextView orderSummaryTextView = (TextView) findViewById(R.id.price_text_view);
-        orderSummaryTextView.setText(message);
-    }
-
 
     }
 
